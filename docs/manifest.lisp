67e9b4a57c31d4ffc785dd9549d142e4d59b2f28
Nick Matvyeyev <mnasoft@gmail.com>
2021-08-15 20:55:42 +0300

    15 авг 2021 г. 17:55:42

--- docs/manifest.lisp
+++ docs/manifest.lisp
@@ -3,6 +3,6 @@
    :systems (:MNAS-ORG-MODE :MNAS-ORG-MODE/DOCS)
    :documents ((:title "Mnas-Org-Mode"
 	        :authors ("Nick Matvyeyev")
-	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :GAMMA) 
+	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :MINIMA) 
                 :sources ("mnas-org-mode.scr" "mnas-org-mode-graph.scr")
                 )))
--- src/docs/docs.lisp
+++ src/docs/docs.lisp
@@ -33,6 +33,7 @@
  Пакет документации формируется в каталоге
 ~/public_html/Common-Lisp-Programs/mnas-org-mode.
 "
+  (mnas-package:make-html-path :mnas-org-mode)
   (make-document)
   (make-graphs)
   (mnas-package:make-mainfest-lisp
