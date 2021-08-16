7cfe6f68c448c4685f83961951ea922c31836dc9
Nick Matvyeyev <mnasoft@gmail.com>
2021-08-16 16:54:24 +0300

    16 авг 2021 г. 13:54:24

--- docs/manifest.lisp
+++ docs/manifest.lisp
@@ -3,6 +3,6 @@
    :systems (:MNAS-ORG-MODE :MNAS-ORG-MODE/DOCS)
    :documents ((:title "Mnas-Org-Mode"
 	        :authors ("Nick Matvyeyev")
-	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :MINIMA) 
+	        :output-format (:TYPE :MULTI-HTML :TEMPLATE :GAMMA) 
                 :sources ("mnas-org-mode.scr" "mnas-org-mode-graph.scr")
                 )))
