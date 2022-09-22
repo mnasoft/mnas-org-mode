ce502cc4d812aca2c36273c7ad47c1967b87aa0e
Nick Matvyeyev <mnasoft@gmail.com>
2022-09-22 08:57:39 +0300

    22 сен 2022 г.  5:57:39

--- src/docs/docs.lisp
+++ src/docs/docs.lisp
@@ -25,26 +25,22 @@
 (defun make-all (&aux
                    (of (if (find (uiop:hostname)
                                  mnas-package:*intranet-hosts*
-                                 :test #'string=)
+                                 :test #'string= :key #'first)
                            '(:type :multi-html :template :gamma)
                            '(:type :multi-html :template :minima))))
-  "@b(Описание:) функция @b(make-all) служит для создания документации.
-
- Пакет документации формируется в каталоге
-~/public_html/Common-Lisp-Programs/mnas-org-mode.
-"
-  (mnas-package:make-html-path :mnas-org-mode)
-  (make-document)
-  (make-graphs)
-  (mnas-package:make-mainfest-lisp
-   '(:mnas-org-mode :mnas-org-mode/docs)
-   "Mnas-Org-Mode"
-   '("Mykola Matvyeyev")
-   (mnas-package:find-sources "mnas-org-mode")
-   :output-format of)
-  (codex:document :mnas-org-mode)
-  (make-graphs)
-  (mnas-package:copy-doc->public-html "mnas-org-mode")
-  (mnas-package:rsync-doc "mnas-org-mode"))
+  (let* ((sys-symbol :mnas-org-mode)
+         (sys-string (string-downcase (format nil "~a" sys-symbol))))
+    (mnas-package:make-html-path sys-symbol)
+    (make-document)
+    (mnas-package:make-mainfest-lisp `(,sys-symbol)
+                                     (string-capitalize sys-string)
+                                     '("Mykola Matvyeyev")
+                                     (mnas-package:find-sources sys-symbol)
+                                     :output-format of)
+    (codex:document sys-symbol)
+    (make-graphs)
+    (mnas-package:copy-doc->public-html sys-string)
+    (mnas-package:rsync-doc sys-string)
+    :make-all-finish))
 
 ;;;; (make-all)
